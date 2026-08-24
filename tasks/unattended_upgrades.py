7293b23abbd3b065923aa7963bd0005e0a9dae8b
maxice8 <thinkabit.ukim@gmail.com>
2026-08-24 07:13:24 -0300

    feat(pyinfra): manage unattended upgrades with confext

--- tasks/unattended_upgrades.py
+++ tasks/unattended_upgrades.py
@@ -1,41 +1,13 @@
-import io
+from pyinfra.operations import apt, systemd
 
-from pyinfra.operations import apt, files, systemd
+from helpers.configuration_extensions import sync_configuration_extension
 
 apt.packages(
     name="Install unattended upgrades",
     packages=["unattended-upgrades"],
 )
 
-files.put(
-    name="Enable periodic unattended upgrades",
-    src=io.StringIO(
-        """APT::Periodic::Enable "1";
-APT::Periodic::Update-Package-Lists "1";
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::AutocleanInterval "7";
-"""
-    ),
-    dest="/etc/apt/apt.conf.d/20auto-upgrades",
-    user="root",
-    group="root",
-    mode="0644",
-)
-
-files.put(
-    name="Configure unattended upgrade cleanup",
-    src=io.StringIO(
-        """Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";
-Unattended-Upgrade::Remove-New-Unused-Dependencies "true";
-Unattended-Upgrade::Remove-Unused-Dependencies "true";
-Unattended-Upgrade::Automatic-Reboot "false";
-"""
-    ),
-    dest="/etc/apt/apt.conf.d/52unattended-upgrades-local",
-    user="root",
-    group="root",
-    mode="0644",
-)
+sync_configuration_extension("unattended-upgrades")
 
 for timer in ("apt-daily.timer", "apt-daily-upgrade.timer"):
     systemd.service(
