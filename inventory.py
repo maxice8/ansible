8672a78490f3fac667501b782cdc85664d98e4c9
maxice8 <thinkabit.ukim@gmail.com>
2026-08-27 08:10:42 -0300

    chore(argocd): update to v3.5.2

--- inventory.py
+++ inventory.py
@@ -4,9 +4,9 @@ servers = [
         {
             # Managed software
             "argocd": {
-                "manifest_sha256": "795a3a972224da6a7f9d32c3e946445f062b60fb46028476715affeb688236e3",
+                "manifest_sha256": "9a87f2b3e14c278f12501eb0ef5c3955b27cf05370ca425381c6a908cf85a5c5",
                 "repository_url": "https://github.com/maxice8/ansible.git",
-                "version": "v3.5.1",
+                "version": "v3.5.2",
             },
             "k3s": {
                 "installer_sha256": "46177d4c99440b4c0311b67233823a8e8a2fc09693f6c89af1a7161e152fbfad",
